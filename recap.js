da4a310b87a947031f22e2021cdf1e9029e2b456
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-26 14:04:28 +0600

    recap of var, let, const, template string, arrow func, destructuring, spread, Oject.keys/values/entries, for-of, for-in

---
+++ recap.js
@@ -0,0 +1,21 @@
+/**
+ * 1. var --> function scoped + changeable
+ * 2. let --> block scoped + changeable
+ * 3. const --> block scoped + nonchangeable
+ * 
+ * 4. template string --> ` ${}`
+ * 
+ * 5. arraw function: () => {return }
+ * or () => ...;
+ * 
+ * 6. destructuring: arr = [1, 2];
+ *                  [x, y] = arr;
+ *      => [..] = array
+ *      => {..} = obj; 
+ * 
+ * 7. spread operation: ...
+ * 8. Object.keys, Object.values, Object.entries
+ * 
+ * 9. for-of: array and string
+ * 10. for-in: object(keys)
+ */
